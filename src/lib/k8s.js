c2d6e79515ccc7a8ef92b86cf6048a760f61ae6b
Markus Wiegand <mail@morphy2k.io>
2018-07-05 16:28:40 +0200

    replace tenxcloud/node-kubernetes-client with godaddy/kubernetes-client

--- src/lib/k8s.js
+++ src/lib/k8s.js
@@ -1,56 +1,28 @@
 'use strict';
 
-const fs = require('fs');
-
-const Client = require('node-kubernetes-client');
+const Client = require('kubernetes-client').Client;
+const clientConfig = require('kubernetes-client').config;
 
 const config = require('./config');
 
 
-const readToken = fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/token');
+const client = new Client({ config: clientConfig.getInCluster() });
 
-const client = new Client({
-  host: config.k8sROServiceAddress,
-  namespace: config.k8sNamespace,
-  protocol: 'https',
-  version: 'v1',
-  token: readToken
-});
+const init = async() => {
+  return await client.loadSpec();
+};
 
-const getMongoPods = () => new Promise((resolve, reject) => {
-  client.pods.get((err, podResult) => {
-    if (err) return reject(err);
-
-    let pods = [];
-    for (let j in podResult) {
-      pods = pods.concat(podResult[j].items);
-    }
-    const labels = config.k8sMongoPodLabelCollection;
-    let results = [];
-    for (let i in pods) {
-      let pod = pods[i];
-      if (podContainsLabels(pod, labels)) {
-        results.push(pod);
-      }
-    }
-
-    resolve(results);
-  });
-});
-
-const podContainsLabels = (pod, labels) => {
-  if (!pod.metadata || !pod.metadata.labels) return false;
-
-  for (let i in labels) {
-    const kvp = labels[i];
-    if (!pod.metadata.labels[kvp.key] || pod.metadata.labels[kvp.key] !== kvp.value) {
-      return false;
-    }
+const getMongoPods = async() => {
+  try {
+    const res = await client.api.v1.namespaces(config.k8sNamespace)
+      .pods.get({ qs: { labelSelector: config.k8sMongoPodLabels } });
+    return res.body.items;
+  } catch (err) {
+    return Promise.reject(err);
   }
-
-  return true;
 };
 
 module.exports = {
+  init,
   getMongoPods
 };
